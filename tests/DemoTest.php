75d3e57ac6f8b282282f964b211a636b0f20a2d5
YiXiang <s9801077@gmail.com>
2017-01-22 14:35:15 +0800

    Add test_for_買了四本不同的書_則會有20off的折扣 Test Case

--- tests/DemoTest.php
+++ tests/DemoTest.php
@@ -25,7 +25,7 @@ class DemoTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($totalPrice, $resultPrice);
     }
 
-    /** 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 */
+    /** 第一集買了一本，第三集也買了一本，價格應為100*2*0.95=190 */
     public function test_for_買了兩本不同的書_則會有5off的折扣()
     {
         /** arrange */
@@ -46,7 +46,7 @@ class DemoTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($totalPrice, $resultPrice);
     }
 
-    /** 一二三集各買了一本，價格應為100*3*0.9=270 */
+    /** 一三五集各買了一本，價格應為100*3*0.9=270 */
     public function test_for_買了三本不同的書_則會有10off的折扣()
     {
         /** arrange */
@@ -58,6 +58,27 @@ class DemoTest extends \PHPUnit\Framework\TestCase
             'fifth' => 1
         ];
         $totalPrice = 270;
+
+        /** act */
+        $Demo = new Demo($data);
+        $resultPrice = $Demo->getPrice();
+
+        /** assert */
+        $this->assertEquals($totalPrice, $resultPrice);
+    }
+
+    /** 一二三四集各買了一本，價格應為100*4*0.8=320 */
+    public function test_for_買了四本不同的書_則會有20off的折扣()
+    {
+        /** arrange */
+        $data = [
+            'first' => 1,
+            'second' => 1,
+            'third' => 1,
+            'fourth' => 1,
+            'fifth' => 0
+        ];
+        $totalPrice = 320;
 
         /** act */
         $Demo = new Demo($data);
