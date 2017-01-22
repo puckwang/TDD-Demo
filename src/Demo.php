a53ad2197a8a7f31aadaaf3ba697c9b883901736
YiXiang <s9801077@gmail.com>
2017-01-22 14:50:25 +0800

    Add feature 需要留意的是，如果你買了四本書，其中三本不同，第四本則是重複的，那麼那三本將享有10%的折扣，但重複的那一本，則仍須100元。

--- src/Demo.php
+++ src/Demo.php
@@ -12,48 +12,64 @@ class Demo
     function __construct(array $data)
     {
         $this->book = $data;
-        foreach ($data as $value) {
+        $this->getSameCount();
+    }
+    private function getSameCount()
+    {
+        $this->book_count = 0;
+        foreach ($this->book as $value) {
             if($value > 0)
                 $this->book_count++;
         }
     }
-
     public function getPrice()
     {
-        switch($this->book_count){
-            case 2:
-                foreach ($this->book as $key => $value) {
-                    $this->totalPrice += $value * $this->unitPrice;
-                    $this->book[$key]--;
-                }
-                $this->totalPrice *= 0.95;
-                break;
-            case 3:
-                foreach ($this->book as $key => $value) {
-                    $this->totalPrice += $value * $this->unitPrice;
-                    $this->book[$key]--;
-                }
-                $this->totalPrice *= 0.90;
-                break;
-            case 4:
-                foreach ($this->book as $key => $value) {
-                    $this->totalPrice += $value * $this->unitPrice;
-                    $this->book[$key]--;
-                }
-                $this->totalPrice *= 0.8;
-                break;
-            case 5:
-                foreach ($this->book as $key => $value) {
-                    $this->totalPrice += $value * $this->unitPrice;
-                    $this->book[$key]--;
-                }
-                $this->totalPrice *= 0.75;
-                break;
-            default:
-                foreach ($this->book as $value) {
-                    $this->totalPrice += $value * $this->unitPrice;
-                }
+        while($this->book_count > 0){
+            $price = 0;
+            switch($this->book_count){
+                case 2:
+                    foreach ($this->book as $key => $value) {
+                        if($value > 0)
+                        $price += $this->unitPrice;
+                        $this->book[$key]--;
+                    }
+                    $price *= 0.95;
+                    break;
+                case 3:
+                    foreach ($this->book as $key => $value) {
+                        if($value > 0)
+                        $price += $this->unitPrice;
+                        $this->book[$key]--;
+                    }
+                    $price *= 0.90;
+                    break;
+                case 4:
+                    foreach ($this->book as $key => $value) {
+                        if($value > 0)
+                        $price += $this->unitPrice;
+                        $this->book[$key]--;
+                    }
+                    $price *= 0.8;
+                    break;
+                case 5:
+                    foreach ($this->book as $key => $value) {
+                        if($value > 0)
+                            $price += $this->unitPrice;
+                        $this->book[$key]--;
+                    }
+                    $price *= 0.75;
+                    break;
+                default:
+                    foreach ($this->book as $key => $value) {
+                        if($value > 0)
+                            $price += $this->unitPrice;
+                        $this->book[$key]--;
+                    }
+            }
+            $this->totalPrice += $price;
+            $this->getSameCount();
         }
+
         return $this->totalPrice;
     }
 }
