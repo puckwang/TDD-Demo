5db6e40867f87305d7f23bb20609546db7ff740e
YiXiang <s9801077@gmail.com>
2017-01-22 14:52:41 +0800

    重構：移除相同功能的片段

--- src/Demo.php
+++ src/Demo.php
@@ -26,45 +26,25 @@ class Demo
     {
         while($this->book_count > 0){
             $price = 0;
+            foreach ($this->book as $key => $value) {
+                if($value > 0){
+                    $price += $this->unitPrice;
+                    $this->book[$key]--;
+                }
+            }
             switch($this->book_count){
                 case 2:
-                    foreach ($this->book as $key => $value) {
-                        if($value > 0)
-                        $price += $this->unitPrice;
-                        $this->book[$key]--;
-                    }
                     $price *= 0.95;
                     break;
                 case 3:
-                    foreach ($this->book as $key => $value) {
-                        if($value > 0)
-                        $price += $this->unitPrice;
-                        $this->book[$key]--;
-                    }
                     $price *= 0.90;
                     break;
                 case 4:
-                    foreach ($this->book as $key => $value) {
-                        if($value > 0)
-                        $price += $this->unitPrice;
-                        $this->book[$key]--;
-                    }
                     $price *= 0.8;
                     break;
                 case 5:
-                    foreach ($this->book as $key => $value) {
-                        if($value > 0)
-                            $price += $this->unitPrice;
-                        $this->book[$key]--;
-                    }
                     $price *= 0.75;
                     break;
-                default:
-                    foreach ($this->book as $key => $value) {
-                        if($value > 0)
-                            $price += $this->unitPrice;
-                        $this->book[$key]--;
-                    }
             }
             $this->totalPrice += $price;
             $this->getSameCount();
